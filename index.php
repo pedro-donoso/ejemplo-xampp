946056ab7c35344c06baef933c96851ae389fd27
pedro-donoso <pedro.donoso.r@usach.cl>
2024-10-01 00:41:57 -0300

    agrego logo

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
         <div id="formContent">
 
             <div class="fadeIn first">
-                <img src="https://th.bing.com/th/id/R.b81c0382fdfc29bc4a6603c1846f0acf?rik=AUACzJrX%2f0VCdA&riu=http%3a%2f%2fpngimg.com%2fuploads%2fphp%2fphp_PNG35.png&ehk=SDq0mYWBBsWE3A6HnxdvAQErErsuHxmn50YjvmaL83Q%3d&risl=&pid=ImgRaw&r=0"
-                    id="icon" alt="User Icon" width="100" height="100"/>
+                <img src="./img/php.png"
+                    id="icon" alt="User Icon" width="150" height="150"/>
                 <h1>PHP News</h1>
             </div>
 

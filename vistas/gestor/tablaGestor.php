3a2c3892581d92a80d08b765a872a52255bb3430
pedro-donoso <pedro.donoso.r@usach.cl>
2024-10-01 10:47:21 -0300

    agrego gestor de archivos

---
+++ vistas/gestor/tablaGestor.php
@@ -0,0 +1,30 @@
+<div class="row">
+    <div class="col-sm-12">
+        <div class="table-responsive">
+            <table>
+                <thead>
+                    <tr>
+                        <td>Nombre</td>
+                        <td>Tipo de archivo</td>
+                        <td>Descargar</td>
+                        <td>Visualizar</td>
+                        <td>Eliminar</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <span class="btn btn-danger btn-sm">
+                            <span class="fa-solid fa-trash"></span>
+                        </span>
+                    </td>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+

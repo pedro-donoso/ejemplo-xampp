605d7fcdd8166760f1dcd8daf09887ae82143c1f
pedro-donoso <pedro.donoso.r@usach.cl>
2024-10-01 12:48:39 -0300

    validacion formulario

--- vistas/footer.php
+++ vistas/footer.php
@@ -3,11 +3,12 @@
  <script src="../librerias/bootstrap/bootstrap.bundle.min.js"></script>
 <script src="../librerias/bootstrap/popper.min.js"></script>
 <script src="../librerias/sweetalert.min.js"></script>
-<script src="../librerias/datatable/jquery-3.7.1.js"></script>
-<script src="../librerias/datatable/bootstrap.min.js"></script>
-<script src="../librerias/datatable/popper.min.js"></script>
-<script src="../librerias/datatable/dataTables.js"></script>
-<script src="../librerias/datatable/dataTables.bootstrap4.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/2.1.7/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/2.1.7/js/dataTables.bootstrap4.js"></script>
 
 </body>
 

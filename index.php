aed74902be8c31ca7a78486e6c22bc14a265d21a
pedro-donoso <pedro.donoso.r@usach.cl>
2024-10-01 01:26:48 -0300

    agrego registro

--- index.php
+++ index.php
@@ -18,22 +18,33 @@
             <div class="fadeIn first">
                 <img src="./img/php.png"
                     id="icon" alt="User Icon" width="150" height="150"/>
-                <h1>PHP News</h1>
+                <h1>Gestor de archivos</h1>
             </div>
 
             <form>
                 <input type="text" id="login" class="fadeIn second" name="login" placeholder="username">
                 <input type="password" id="password" class="fadeIn third" name="login" placeholder="password">
-                <input type="submit" class="fadeIn fourth" value="Log In">
+                <input type="submit" class="fadeIn fourth" value="Entrar">
             </form>
 
             <div id="formFooter">
-                <a class="underlineHover" href="#">Go to the Site</a>
+                <a class="underlineHover" href="registro.php">Registrar</a>
             </div>
 
         </div>
 
     </div>
+
+
+    <script src="librerias/sweetalert.min.js"></script>
+
+    <script>
+        document.getElementById("loginForm").addEventListener("submit", function(event){
+            event.preventDefault();
+            swal("Good job!", "You clicked the button!", "success");
+        });
+    </script>
+
 </body>
 
 </html>

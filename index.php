af438aa6f7abc22e3876776f809e2609591317a4
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-30 23:45:24 -0300

    agrego imagen

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
             <div class="fadeIn first">
                 <img src="https://th.bing.com/th/id/R.b81c0382fdfc29bc4a6603c1846f0acf?rik=AUACzJrX%2f0VCdA&riu=http%3a%2f%2fpngimg.com%2fuploads%2fphp%2fphp_PNG35.png&ehk=SDq0mYWBBsWE3A6HnxdvAQErErsuHxmn50YjvmaL83Q%3d&risl=&pid=ImgRaw&r=0"
-                    id="icon" alt="User Icon" />
+                    id="icon" alt="User Icon" width="100" height="100"/>
                 <h1>Aditya News</h1>
             </div>
 

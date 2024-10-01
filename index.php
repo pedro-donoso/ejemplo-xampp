27723c558d07856ee174969c97e98e8c93366406
pedro-donoso <pedro.donoso.r@usach.cl>
2024-10-01 00:02:30 -0300

    agrego estilos css

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="./css/login.css">
 </head>
 
 <body>
@@ -14,7 +15,7 @@
             <div class="fadeIn first">
                 <img src="https://th.bing.com/th/id/R.b81c0382fdfc29bc4a6603c1846f0acf?rik=AUACzJrX%2f0VCdA&riu=http%3a%2f%2fpngimg.com%2fuploads%2fphp%2fphp_PNG35.png&ehk=SDq0mYWBBsWE3A6HnxdvAQErErsuHxmn50YjvmaL83Q%3d&risl=&pid=ImgRaw&r=0"
                     id="icon" alt="User Icon" width="100" height="100"/>
-                <h1>Aditya News</h1>
+                <h1>PHP News</h1>
             </div>
 
             <form>

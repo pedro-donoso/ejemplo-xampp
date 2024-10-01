aeede4ab1955d3465e0b4d7065012575e689923d
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-30 22:54:24 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div class="wrapper fadeInDown">
+        <div id="formContent">
+
+            <div class="fadeIn first">
+                <img src="https://www.b-cube.in/wp-content/uploads/2014/05/aditya-300x177.jpg" id="icon" alt="User Icon"/>
+                <h1>Aditya News</h1>
+            </div>
+
+            <form>
+                <input type="text" id="login" class="fadeIn second" name="login" placeholder="username">
+                <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+                <input type="submit" class="fadeIn fourth" value="Log In">
+            </form>
+
+            <div id="formFooter">
+                <a class="underlineHover" href="#">Go to the Site</a>
+            </div>
+
+        </div>
+
+    </div>
+</body>
+</html>

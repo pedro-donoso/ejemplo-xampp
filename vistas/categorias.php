9f73950cb37d981042153b6787aef5772dc40da4
pedro-donoso <pedro.donoso.r@usach.cl>
2024-10-02 14:18:28 -0300

    agrego tabla categorias

--- vistas/categorias.php
+++ vistas/categorias.php
@@ -8,14 +8,33 @@ if (isset($_SESSION["usuario"])) {
 
     <div class="jumbotron jumbotron-fluid">
         <div class="container">
-            <h1 class="display-4">Categorias</h1>
-
+            <h1 class="display-4">Categorías</h1>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div id="tablaCategorias"></div>
+                </div>
+            </div>
         </div>
     </div>
 
     <?php
     include "footer.php";
+    ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('#tablaCategorias').load('categorias/tablaCategoria.php');
+        });
+    </script>
+
+<?php 
+
 } else {
     header("location:../index.php");
 }
+
 ?>
+
+
---
+++ vistas/categorias/tablaCategoria.php
@@ -0,0 +1,29 @@
+<div class="table-responsive">
+    <table class="table table-hover table-dark">
+        <thead>
+            <tr>
+                <td>Nombre</td>
+                <td>Fecha</td>
+                <td>Editar</td>
+                <td>Eliminar</td>  
+            </tr>
+            
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>
+                    <span class="btn btn-primary btn-sm">
+                    <span class="fa-solid fa-pen-to-square"></span>
+                    </span>
+                </td>
+                <td>
+                    <span class="btn btn-primary btn-sm">
+                        <span class="fa-solid fa-trash"></span>
+                    </span>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
